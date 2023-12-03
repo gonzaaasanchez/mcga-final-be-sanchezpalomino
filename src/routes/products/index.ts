d091681dc069b9ced4d9659ca87d14f51c23742e
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-12-03 12:28:21 -0300

    DELETE unnecessary comments

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -3,19 +3,10 @@ import productsController from '../products/index.js';
 
 const productsRouter = Router();
 
-//GET
 productsRouter.get('/', productsController.getAll);
-
-//POST
 productsRouter.post('/', productsController.create);
-
-//GET
 productsRouter.get('/:id', productsController.getById);
-
-//DELETE
 productsRouter.delete('/:id', productsController.delete);
-
-//PUT
 productsRouter.put('/:id', productsController.update);
 
 export default productsRouter;
