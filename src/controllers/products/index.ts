de40937c2372e592f6ed81fab239ef8213bad804
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-12-03 12:26:30 -0300

    ADD create in products controller

--- src/controllers/products/index.ts
+++ src/controllers/products/index.ts
@@ -11,7 +11,7 @@ const productsController = {
             data: allProducts,
         });
     },
-
+    
     getById: async (_req: Request, res: Response) => {
         try {
             const product = await ProductModel.findById(_req.params.id);
@@ -19,6 +19,28 @@ const productsController = {
                 return res.status(200).json({
                     status: 200,
                     data: product,
+                });
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(400).json({
+                    message: error.message,
+                    error: true,
+                });
+            }
+        }
+    },
+
+    create: async (_req: Request, res: Response) => {
+        try {
+            const newProduct = new ProductModel({ ..._req.body });
+            const product = await newProduct.save();
+
+            if (product) {
+                return res.status(201).json({
+                    message: 'Product successfully created',
+                    data: product,
+                    error: false,
                 });
             }
         } catch (error) {
