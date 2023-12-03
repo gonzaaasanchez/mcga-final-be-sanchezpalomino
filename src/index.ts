d65d7f4bbd7c69c8ddcba8b8807d11697d26d9c0
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-12-03 12:21:11 -0300

    ADD index.ts

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import app from './app';
+import * as mongoose from 'mongoose';
+
+const port = 3000 || 8080;
+
+const MONGODB_URL =
+  'mongodb+srv://gonzalosanchez:hola123123@cluster0.zrttfst.mongodb.net/mcga-tp?retryWrites=true&w=majority' ||
+  '';
+
+mongoose
+    .connect(MONGODB_URL)
+    .then(() => {
+        console.log({
+            level: 'info',
+            message: '✅ Database connected',
+            label: 'mongo',
+        });
+        app.listen(port, () => {
+            console.log({
+                level: 'info',
+                message: `Server listening on port ${port}`,
+                label: 'server',
+            });
+        });
+    })
+    .catch((error) =>
+        console.log({
+            level: 'error',
+            message: '🔴 Database error: ',
+            errorData: error,
+            label: 'mongo',
+        }),
+    );
