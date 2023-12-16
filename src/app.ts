e4f46ef05cde978d64dd50fdc13f7dafb12b9666
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2023-12-16 12:39:09 -0300

    ADD use cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Express, Response } from 'express';
+import cors from 'cors';
 import productRouter from './routes/products/index';
 
 const app: Express = express();
 app.use(express.json());
+app.use(cors());
 
 // app routes
 app.use('/products', productRouter);
